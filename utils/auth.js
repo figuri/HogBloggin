69459d11f8f704e567dddf005a94db9bba64a443
unknown <samtisme@gmail.com>
2023-08-10 20:02:32 -0700

    added some routes and a user model

---
+++ utils/auth.js
@@ -0,0 +1,23 @@
+const withAuth = (req, res, next) => {
+   
+    if (!req.session.loggedIn) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  
+  const areAuth = (req, res, next) => {
+ 
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    } else {
+      next();
+    }
+  };
+  
+  
+  module.exports = { withAuth, areAuth };
+  

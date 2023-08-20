93396f75189ebfec291c7d9637e928201e0b112e
unknown <samtisme@gmail.com>
2023-08-20 01:06:10 -0700

    added some seed information and changed some files

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -7,19 +7,25 @@ const seedDatabase = async () => {
         await sequelize.query('SET FOREIGN_KEY_CHECKS = 0');
         await sequelize.sync({ force: true });
         await sequelize.query('SET FOREIGN_KEY_CHECKS = 1');
-        await User.bulkCreate(data.users, {
-            individualHooks: true,
-            returning: true,
-        });
-        await Post.bulkCreate(data.posts, {
-            individualHooks: true,
-            returning: true,
-        });
-        process.exit(0);
+        await User.create(data[0]);
+        await User.create(data[1]);
+        await User.create(data[2]);
+        await Post.create(data[3]);
+        await Post.create(data[4]);
+        await Post.create(data[5]);
+        await Post.create(data[6]);
+        await Post.create(data[7]);
+        await Post.create(data[8]);
+        await Post.create(data[9]);
+        await Post.create(data[10]);
+        await Post.create(data[11]);
+        await Post.create(data[12]);
+        await Post.create(data[13]);
+        await Post.create(data[14]);
     } catch (err) {
         console.log(err);
-        process.exit(1);
     }
+    process.exit(0);
 };
 
 seedDatabase();

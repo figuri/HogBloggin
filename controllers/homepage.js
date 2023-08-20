d6e96b20da34342ba842f8a311ccb42b4a870e6f
unknown <samtisme@gmail.com>
2023-08-20 02:22:15 -0700

    changed session object in server file so user session times out after ten minutes

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { User, Post, Comment } = require('../models');
 
-router.get('/', async (req, res) => {
+// get route to display all posts from all users (homepage)
+
+router.get('/home', async (req, res) => {
     try {
         const postData = await Post.findAll({
             include: [{ model: User }],
@@ -17,6 +19,57 @@ router.get('/', async (req, res) => {
         console.log(err);
     }
 });
+
+// make a post
+
+router.post('/create', async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+            title: req.body.title,
+            post_text: req.body.post_text,
+            date_created: req.body.date_created,
+            });
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// edit a post by ID
+
+router.put('/update/:id', async (req, res) => {
+    try {
+        const updatedPost = await Post.update(req.body, {
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+                },
+            });
+        res.status(200).json(updatedPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// delete a post by ID
+
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const deletedPost = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+                },
+            });
+        res.status(200).json(deletedPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// get route to display dashboard where user can create/edit/delete posts
 
 router.get('/dashboard', async (req, res) => {
     try {
@@ -33,22 +86,39 @@ router.get('/dashboard', async (req, res) => {
     }
   });
 
-router.get('/dashboard', async (req, res) => {
-    try {
-        const userData = await User.findByPk(req.session.user_id, {
-                include: [{ model: Post }],
-                });
-        const user = userData.get({ plain: true });
-        res.render('dashboard', {
-            ...user,
-            loggedIn: req.session.loggedIn,
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-}
-);
+//   route to make a comment on a post
 
+router.post('/comment', async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            user_id: req.session.user_id,
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            });
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// route to delete a comment by ID only if you are the user who made the comment
+
+router.delete('/comment/:id', async (req, res) => {
+    try {
+        const deletedComment = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+                },
+            });
+        res.status(200).json(deletedComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// login route
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
@@ -57,7 +127,7 @@ router.get('/login', (req, res) => {
     res.render('login');
 }
 );
-
+// signup route
 router.get('/signup', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
-
+// set up app and port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -15,27 +15,26 @@ const PORT = process.env.PORT || 3001;
 // Path: server.js
 // Compare this snippet from utils\auth.js:
 
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
+// use this create a new Sequelize Store to store the session data
+app.use(session({
+    secret: 'Secret code you will never guess',
     resave: false,
-    saveUninitialized: false,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
-
+    saveUninitialized: true,
+    cookie: { maxAge: 10 * 60 * 1000 } // 10 minutes
+  }));
+// middleware to make session info available to all of our Handlebars templates
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.use(session(sess));
-
+// middleware to parse JSON and string data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// turn on routes
 app.use(routes);
 
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('We have lift off'));
 });
